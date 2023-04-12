c0b0df6adbbbd06a9aa655dfeefe4196185d6265
charlesanderson25 <charles_spfc@hotmail.com>
2023-04-12 13:01:35 -0300

    Inclusão do onclick

--- about.js
+++ about.js
@@ -1,26 +1,17 @@
-const tabLinks = document.querySelectorAll('.tab-links');
-const tabContents = document.querySelectorAll('.tab-contents');
+const tabLinks = document.getElementsByClassName('tab-links');
+const tabContents = document.getElementsByClassName('tab-contents');
 
-for (let i = 0; i < tabLinks.length; i++) {
-  tabLinks[i].addEventListener('click', function() {
-    for (let j = 0; j < tabContents.length; j++) {
-      tabContents[j].classList.remove('active-tab');
-    }
-    // const id = this.getAttribute('id');
-    // const content = document.querySelector(`.tab-contents:nth-of-type(${id.slice(-1)})`);
-    // content.classList.add('active-tab');
-  });
+function opentab(tabname){
+  for(tabLink of tabLinks){
+    tabLink.classList.remove('active-link');
+  }
+  for(tabContent of tabContents){
+    tabContent.classList.remove('active-tab');
+  }
+  event.currentTarget.classList.add('active-link');
+  document.getElementById(tabname).classList.add('active-tab');
 }
 
-var tablinks = document.getElementsByClassName('tab-links');
-var tabcontents = document.getElementsByClassName('tab-contents');
 
-function opentab(tabnmae){
-    for (tablink of tablinks){
-        tablink.classList.remove("active-link");
-    }
-    for (tabcontent of tabcontents){
-        tabcontent.classList.remove("active-tab");
-    }
-    event.currentTarget.classList.add("active-link");
-}
+
+
