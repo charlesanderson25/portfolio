a18e26522e68d739d68e00468bb3feac1acab085
charlesanderson25 <charles_spfc@hotmail.com>
2023-04-15 17:54:49 -0300

    Skills transforma Objeto adiciona item na li

--- skills.js
+++ skills.js
@@ -1,27 +1,43 @@
 // Selecionando itens da lista de skills data e transformando em um Array
 
-const listaSkills = document.querySelectorAll('#data-list-skills li');
-const arrayListaWeb = [];
+    const listaSkillsData = document.querySelectorAll('#data-list-skills li');
+    const arrayListaData = [];
 
-listaSkills.forEach(item =>{
-    arrayListaWeb.push(item.textContent.trim());
-});
+    listaSkillsData.forEach(item =>{
+        arrayListaData.push(item.textContent.trim());
+    });
 
-//Adicionando novo elemento a Li
+        //Adicionando novo elemento a Li
 
-const newLiData = document.createElement('li');
-newLiData.textContent = 'MySQL';
-document.getElementById('data-list-skills').appendChild(newLiData);
+        const newLiData = document.createElement('li');
+        newLiData.textContent = 'MySQL';
+        document.getElementById('data-list-skills').appendChild(newLiData);
+
+// Selecionando itens da lista de skills Web e transformando em um Objeto
+
+    const listaSkillWeb = document.querySelectorAll('web-list-skills li');
+    const objetoListaWeb = {};
+
+    listaSkillWeb.forEach(item => {
+        const itemName = item.textContent.trim();
+        objetoListaWeb[itemName] = true;
+    });
+
+        //Adicionando novo elemento no Objeto
+        const newLiWeb = document.createElement('li');
+        newLiWeb.textContent = 'MySQL';
+        objetoListaWeb['MySQL'] = newLiWeb;
+        document.getElementById('web-list-skills').appendChild(newLiWeb);
 
 // Trocar/alternar conteúdo página skills a cada 5 segundos
 
-setInterval(() => {
-    const skills1 = document.querySelector('#skills-web');
-    const skills2 = document.querySelector('#skills-data');
-    const container = document.querySelector('#secondary-web');
+    setInterval(() => {
+        const skills1 = document.querySelector('#skills-web');
+        const skills2 = document.querySelector('#skills-data');
+        const container = document.querySelector('#secondary-web');
 
-    container.insertBefore(skills2, skills1);
-}, 5000);
+        container.insertBefore(skills2, skills1);
+    }, 5000);
 
 
 
